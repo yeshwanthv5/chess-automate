da9d6d5e51668f213381836fb980deac529c6c26
Yeshwanth Venkatesha <yeshwanth.venkatesha@yale.edu>
2021-03-24 23:55:09 -0400

    Add some constants of the game. Generate all combs

--- utils/combinations_pieces.py
+++ utils/combinations_pieces.py
@@ -1,9 +1,12 @@
-combinations = 0
+combinations = []
+count = 0
 for pawns in range(6, 11):
     for knights in range(0, 10):
         for bishops in range(0, 10):
             for rooks in range(0, 6):
                 for queens in range(0, 5):
-                    if pawns + 3*knights + 3*bishops + 5*rooks + 7*queens == 35:
-                        combinations += 1
+                    if pawns + 3*knights + 3*bishops + 4*rooks + 7*queens == 35:
+                        combinations.append([pawns, knights, bishops, rooks, queens])
+                        count += 1
 print(combinations)
+print(count)
